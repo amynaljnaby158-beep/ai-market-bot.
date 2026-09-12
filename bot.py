107360b5d679cafec87e0506312350a84855d837
amynaljnaby158-beep <amynaljnaby158@gmail.com>
2026-09-12 19:39:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,2 +1,10 @@
 from telegram import Bot
 bot = Bot("8893640950:AAH9sFZJuO4KJLiswD2jRGHBl2p_FqMDigo")
+print("البوت جاهز")
+from telegram.ext import Application, CommandHandler
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+async def start(update, context):
+      await update.message.reply_text("هلا بيك، البوت شغال 🤖")
+  app = Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.run_polling()
